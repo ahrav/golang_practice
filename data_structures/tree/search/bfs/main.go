672644d2c05f5e78bf2dc7dcb070f87b42c85fe9
ahrav <ahrav.dutta@leftfieldlabs.com>
2021-08-25 06:38:42 -0700

    More DFS and BFS, but mostly DFS.

--- data_structures/tree/search/bfs/main.go
+++ data_structures/tree/search/bfs/main.go
@@ -8,7 +8,7 @@ import (
 type Node struct {
 	Value int
 	Children []*Node
-	Right, Left *Node
+	Right, Left, Next *Node
 }
 
 func (n *Node) BreadthFirstSearch(arr []int) {
@@ -31,9 +31,9 @@ func (n *Node) TraversePrint() [][]int {
 	}
 	s := []*Node{n}
 	for len(s) > 0 {
-		level := len(s)
+		levelSize := len(s)
 		var arr []int
-		for i:=0; i<level; i++ {
+		for i:=0; i<levelSize; i++ {
 			node := s[0]
 			arr = append(arr, node.Value)
 			s = s[1:]
@@ -49,6 +49,107 @@ func (n *Node) TraversePrint() [][]int {
 	return res
 }
 
+// ConnectLevelOrder connects each node to its level order successor.
+func (n *Node) ConnectLevelOrder() {
+	if n == nil {
+		return
+	}
+	s := []*Node{n}
+	for len(s) > 0 {
+		levelSize := len(s)
+		for i:=0; i<levelSize; i++ {
+			node := s[0]
+			if i == levelSize-1 {
+				node.Next = nil
+			} else {
+				node.Next = s[1]
+			}
+			s = s[1:]
+			if node.Left != nil {
+				s = append(s, node.Left)
+			}
+			if node.Right != nil {
+				s = append(s, node.Right)
+			}
+		}
+	}
+}
+
+// ConnectAllLevelOrder connects each node to its level order sibling.
+func (n *Node) ConnectAllLevelOrder() {
+	if n == nil {
+		return
+	}
+	s := []*Node{n}
+	for len(s) > 0 {
+		node := s[0]
+		s = s[1:]
+		if node.Right != nil {
+			s = append(s, node.Right)
+		}
+		if node.Left != nil {
+			s = append(s, node.Left)
+		}
+		if len(s) > 0 {
+			node.Next = s[0]
+		} else {
+			node.Next = nil
+		}
+	}
+}
+
+// RightView returns all nodes that are the last node on each level.
+func (n *Node) RightView() []Node {
+	if n == nil {
+		return nil
+	}
+	var res []Node
+	s := []*Node{n}
+	for len(s) > 0 {
+		levelSize := len(s)
+		for i:=0; i<levelSize; i++ {
+			node := s[0]
+			if i == levelSize-1 {
+				res = append(res, *node)
+			}
+			s = s[1:]
+			if node.Right != nil {
+				s = append(s, node.Right)
+			}
+			if node.Left != nil {
+				s = append(s, node.Left)
+			}
+		}
+	}
+	return res
+}
+
+// TraverseAvgPrint will print each level's average.
+func (n *Node) TraverseAvgPrint() []float64 {
+	var res []float64
+	if n == nil {
+		return res
+	}
+	s := []*Node{n}
+	for len(s) > 0 {
+		levelSize := len(s)
+		var sum int
+		for i:=0; i<levelSize; i++ {
+			node := s[0]
+			sum+= node.Value
+			s = s[1:]
+			if node.Left != nil {
+				s = append(s, node.Left)
+			}
+			if node.Right != nil {
+				s = append(s, node.Right)
+			}
+		}
+		res = append(res, float64(sum)/float64(levelSize))
+	}
+	return res
+}
+
 // TraversePrintReverse will print each level of the tree as a slice.
 func (n *Node) TraversePrintReverse() [][]int {
 	var res [][]int
@@ -57,9 +158,9 @@ func (n *Node) TraversePrintReverse() [][]int {
 	}
 	s := []*Node{n}
 	for len(s) > 0 {
-		level := len(s)
+		levelSize := len(s)
 		var arr []int
-		for i:=0; i<level; i++ {
+		for i:=0; i<levelSize; i++ {
 			node := s[0]
 			arr = append(arr, node.Value)
 			s = s[1:]
@@ -75,18 +176,131 @@ func (n *Node) TraversePrintReverse() [][]int {
 	return res
 }
 
-func (n *Node) traverse() {
-	if n != nil {
-		s := []*Node{n}
-		for len(s) > 0 {
+// TraverseZigZagPrint will print each level of the tree as a slice.
+func (n *Node) TraverseZigZagPrint() [][]int {
+	var res [][]int
+	if n == nil {
+		return res
+	}
+	s := []*Node{n}
+	for len(s) > 0 {
+		levelSize := len(s)
+		var arr []int
+		for i:=0; i<levelSize; i++ {
 			node := s[0]
-			fmt.Printf(strconv.Itoa(node.Value) + " ")
+			if levelSize%2 == 0 {
+				arr = append([]int{node.Value}, arr...)
+			} else {
+				arr = append(arr, node.Value)
+			}
 			s = s[1:]
 			if node.Left != nil {
 				s = append(s, node.Left)
 			}
 			if node.Right != nil {
 				s = append(s, node.Right)
+			}
+		}
+		res = append(res, arr)
+	}
+	return res
+}
+
+// MinDepth will return shortest depth of the tree.
+func (n *Node) MinDepth() int {
+	if n == nil {
+		return 0
+	}
+	if n.Left == nil || n.Right == nil {
+		return 1
+	}
+	s := []*Node{n}
+	var level int
+	for len(s) > 0 {
+		level++
+		levelSize := len(s)
+		for i:=0; i<levelSize; i++ {
+			node := s[0]
+			s = s[1:]
+			if node.Left == nil && node.Right == nil {
+				return level
+			}
+			if node.Left != nil {
+				s = append(s, node.Left)
+			}
+			if node.Right != nil {
+				s = append(s, node.Right)
+			}
+		}
+	}
+	return level
+}
+
+// MaxDepth will return shortest depth of the tree.
+func (n *Node) MaxDepth() int {
+	if n == nil {
+		return 0
+	}
+	if n.Left == nil || n.Right == nil {
+		return 1
+	}
+	s := []*Node{n}
+	var level int
+	for len(s) > 0 {
+		level++
+		levelSize := len(s)
+		for i:=0; i<levelSize; i++ {
+			node := s[0]
+			s = s[1:]
+			if node.Left != nil {
+				s = append(s, node.Left)
+			}
+			if node.Right != nil {
+				s = append(s, node.Right)
+			}
+		}
+	}
+	return level
+}
+
+// LevelOrderSuccesor will return the node after given node.
+func (n *Node) LevelOrderSuccesor(v *Node) *Node {
+	if n == nil {
+		return nil
+	}
+	s := []*Node{n}
+	for len(s) > 0 {
+		node := s[0]
+		s = s[1:]
+		if node.Right != nil {
+			s = append(s, node.Right)
+		}
+		if node.Left != nil {
+			s = append(s, node.Left)
+		}
+		if node.Value == v.Value {
+			break
+		}
+	}
+	return s[0]
+}
+
+func (n *Node) traverse() {
+	if n != nil {
+		s := []*Node{n}
+		for len(s) > 0 {
+			node := s[0]
+			var val int
+			if node.Next != nil {
+				val = node.Next.Value
+			}
+			fmt.Printf(strconv.Itoa(node.Value) + " " + strconv.Itoa(val) + " ")
+			s = s[1:]
+			if node.Right != nil {
+				s = append(s, node.Right)
+			}
+			if node.Left != nil {
+				s = append(s, node.Left)
 			}
 		}
 	}
@@ -101,14 +315,28 @@ func (n *Node) postorder() {
 }
 
 func main() {
-		root := &Node{Value: 12}
-		root.Left = &Node{Value: 7}
-		root.Right = &Node{Value: 1}
-		root.Left.Left = &Node{Value: 9}
-		root.Right.Left = &Node{Value: 10}
-		root.Right.Right = &Node{Value: 5}
-		fmt.Println(root.TraversePrint())
-		fmt.Println(root.TraversePrintReverse())
-		root.traverse()
-		root.postorder()
+		// node := &Node{Value: 3}
+		root := &Node{Value: 1}
+		root.Left = &Node{Value: 3}
+		root.Right = &Node{Value: 2}
+		root.Left.Left = &Node{Value: 7}
+		root.Right.Left = &Node{Value: 5}
+		root.Right.Right = &Node{Value: 4}
+		root.Left.Right = &Node{Value: 6}
+		// root.Right.Left.Right = &Node{Value: 17}
+		// fmt.Println(root.TraversePrint())
+		// fmt.Println(root.TraversePrintReverse())
+		// fmt.Println(root.TraverseZigZagPrint())
+		// fmt.Println(root.TraverseAvgPrint())
+		// fmt.Println(root.MinDepth())
+		// fmt.Println(root.MaxDepth())
+		// fmt.Println(root.LevelOrderSuccesor(node))
+		// root.ConnectLevelOrder()
+		// root.ConnectAllLevelOrder()
+		// root.traverse()
+		fmt.Println(root.RightView())
+		// fmt.Println(root.TraversePrint())
+		// root.traverse()
+		// root.traverse()
+		// root.postorder()
 }
--- data_structures/tree/search/dfs/main.go
+++ data_structures/tree/search/dfs/main.go
@@ -1,8 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 type Node struct {
 	Value int
 	Children []*Node
+	Right, Left, Next *Node
 }
 
 func (n *Node) DepthFirstSearch(arr []int) []int {
@@ -11,4 +17,163 @@ func (n *Node) DepthFirstSearch(arr []int) []int {
 		arr = child.DepthFirstSearch(arr)
 	}
 	return arr
+}
+
+// SumPath returns whether there is a path from the root to a leaf node that sums to a value.
+func (n *Node) SumPath(sum int) bool {
+	if n == nil {
+		return false
+	}
+	if n.Value == sum && n.Left == nil && n.Right == nil {
+		return true
+	}
+	return n.Right.SumPath(sum - n.Value) || n.Left.SumPath(sum - n.Value)
+}
+
+// AllSumPath returns all paths in the tree from the node to a leaf that equals a value.
+func (n *Node) AllSumPath(sum int) [][]int {
+	s := time.Now()
+	var p [][]int
+	var a []int
+	n.recursivePaths(sum, a, &p)
+	e := time.Since(s)
+	fmt.Printf("recursion1 : %s", e)
+	return p
+}
+
+// AllSumPath2 returns all paths in the tree from the node to a leaf that equals a value.
+func (n *Node) AllSumPath2(sum int) [][]int {
+	s := time.Now()
+	var p [][]int
+	var a []int
+	n.recursivePaths2(sum, a, &p)
+	e := time.Since(s)
+	fmt.Printf("recursion2 : %s", e)
+	return p
+}
+
+func (n *Node) recursivePaths(sum int, path []int, allPaths *[][]int) {
+	if n == nil {
+		return
+	}
+
+	path = append(path, n.Value)
+	if n.Value == sum && n.Left == nil && n.Right == nil {
+		*allPaths = append(*allPaths, path)
+		path = path[:len(path)-1]
+	} else {
+		n.Right.recursivePaths(sum - n.Value, path, allPaths)
+		n.Left.recursivePaths(sum - n.Value, path, allPaths)
+	}
+}
+
+func (n *Node) recursivePaths2(sum int, path []int, allPaths *[][]int) {
+	if n == nil {
+		return
+	}
+
+	path = append(path, n.Value)
+	if n.Value == sum && n.Left == nil && n.Right == nil {
+		*allPaths = append(*allPaths, append([]int{}, path...))
+		path = path[:len(path)-1]
+	} else {
+		n.Right.recursivePaths(sum - n.Value, path, allPaths)
+		n.Left.recursivePaths(sum - n.Value, path, allPaths)
+	}
+}
+
+// SumNumberPath gets the sum of each DFS path.
+func (n *Node) SumNumberPath() int {
+	s := time.Now()
+	var p [][]int
+	var a []int
+	n.recursivePath(a, &p)
+	var sum int
+	for _, x := range p {
+		sum += sliceToInt(x)
+	}
+	e := time.Since(s)
+	fmt.Printf("SumNumberPath time: %s", e)
+	return sum
+}
+
+// SumNumberPath2 gets the sum of each DFS path.
+func (n *Node) SumNumberPath2() int {
+	s := time.Now()
+	e := time.Since(s)
+	fmt.Printf("SumNumberPath2 time: %s", e)
+	return n.rootToLeafPath(0)
+}
+
+func (n *Node) rootToLeafPath(sum int) int {
+	if n == nil {
+		return 0
+	}
+
+	sum = 10 * sum + n.Value
+	if n.Left == nil && n.Right == nil {
+		return sum
+	}
+	return n.Left.rootToLeafPath(sum) + n.Right.rootToLeafPath(sum)
+}
+
+func (n *Node) recursivePath(path []int, allPaths *[][]int) {
+	if n == nil {
+		return
+	}
+	path = append(path, n.Value)
+	if n.Left == nil && n.Right == nil {
+		*allPaths = append(*allPaths, append([]int{}, path...))
+		path = path[:len(path)-1]
+	} else {
+		n.Right.recursivePath(path, allPaths)
+		n.Left.recursivePath(path, allPaths)
+	}
+}
+
+// PathSequence will determine if a given sequence exists from root to leaf.
+func (n *Node) PathSequence(seq []int) bool {
+	if n == nil {
+		return len(seq) == 0
+	}
+	return n.checkPath(seq, 0)
+}
+
+func (n *Node) checkPath(seq []int, idx int) bool {
+	if n == nil || n.Value != seq[idx] {
+		return false
+	}
+	if n.Right == nil && n.Left == nil {
+		return true
+	}
+	idx++
+	return n.Right.checkPath(seq, idx) || n.Left.checkPath(seq, idx)
+}
+
+func sliceToInt(s []int) int {
+	res := 0
+	op := 1
+	for i:=len(s)-1; i>=0; i-- {
+		res += s[i] * op
+		op *= 10
+	}
+	return res
+}
+
+func main() {
+	// node := &Node{Value: 3}
+	root := &Node{Value: 1}
+	root.Left = &Node{Value: 9}
+	root.Right = &Node{Value: 7}
+	root.Left.Left = &Node{Value: 9}
+	root.Right.Left = &Node{Value: 5}
+	root.Right.Right = &Node{Value: 4}
+	root.Left.Right = &Node{Value: 2}
+	// root.Right.Left.Right = &Node{Value: 17}
+	fmt.Println(root.SumPath(11))
+	fmt.Println(root.AllSumPath(12))
+	fmt.Println(root.AllSumPath2(12))
+	fmt.Println(root.SumNumberPath())
+	fmt.Println(root.SumNumberPath2())
+	fmt.Println(root.PathSequence([]int{1, 9, 9}))
 }
